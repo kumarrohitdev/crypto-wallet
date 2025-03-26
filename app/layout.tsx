06dd097b28b8b12c7cb44925e177e0508619540d
kumarrohitdev <rohitsharma001914@gmail.com>
2025-03-26 16:37:29 +0530

    added home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// @ts-ignore
+import { Navbar } from "@/components/ui/navbar";
 
 export const metadata: Metadata = {
   title: "CryptoWay",
@@ -25,8 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
